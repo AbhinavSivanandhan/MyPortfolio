87b2813027df0a1c2bca16cae17d1df93d46e919
Abhinav Sivanandhan <abhinav.sivanandhan@gmail.com>
2023-10-01 18:07:23 -0400

    hero section updated

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,15 @@ import styled from "styled-components"
 
 const Container = styled.div`
   height: 100vh;
-  background-color: rebeccapurple;
+  scroll-snap-type: y mandatory;
+  scroll-behavior: smooth;
+  overflow-y: auto;
+  scrollbar-width: none;
+  color: grey;
+  background: url("./img/bg.png");
+  &&::-webkit-scrollbar{
+    display: none;
+  }
 `
 function App() {
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Section = styled.div`
+  display: flex;
+  justify-content: center;
+`
+const Container = styled.div`
+  width: 1400px;
+  background-color: ivory;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+`
+const Links = styled.div`
+display: flex;
+align-items: center;
+gap: 50px;
+`
+const Logo = styled.img`
+`
+const List = styled.ul`
+display: flex;
+gap: 20px;
+list-style: none;
+`
+const ListItem = styled.li`
+cursor: pointer;
+`
+const Icons = styled.div`
+`
+const Icon = styled.img`
+`
+const Button = styled.button`
+width: 100px;
+padding: 10px;
+background-color: pink;
+color: white;
+border: none;
+border-radius: 5px;
+`
+
+const Navbar = () => {
+    return (
+        <Section>
+          <Container>
+            <Links>
+            <Logo src="./img/logo.png" width="300" height="120"/>
+            <List>
+              <ListItem>Home</ListItem>
+              <ListItem>Studio</ListItem>
+              <ListItem>Works</ListItem>
+              <ListItem>Contact</ListItem>
+            </List>
+            <Icons>
+              <Icon src="./img/hire.png" width="20" height="20"/>
+              <Button>Hire Now</Button>
+            </Icons>
+            </Links>
+          </Container>
+        </Section>
+    )
+}
+
+export default Navbar
